1fc55164e4c99ad8981cbc80bd7ae69034bf38b1
silight <silvinlight@gmail.com>
2015-09-27 19:41:46 -0400

    Added Admin functionality. Added HTML.

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'author', 'created_date', 'published_date')
+	list_filter = ['published_date', 'created_date']
+	search_fields = ['question_text', 'title', 'author']
+
+
+admin.site.register(Post, PostAdmin)
